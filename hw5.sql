a815e93d2fa75e3d2c1569c823ad69982ed51d26
Kate <you@example.com>
2022-01-22 23:41:57 +0300

    change

--- hw5.sql
+++ hw5.sql
@@ -15,21 +15,3 @@ to_char(cte2.actual_departure,'dd.mm.yyyy')
 order by cte2.departure_airport, cte2.actual_departure)
 from cte1 
 join cte2 on cte1.flight_id = cte2.flight_id
-
-with cte1 as (
-select count(status) as real_flights, aircraft_code
-from bookings.flights
-where status not in ('Cancelled', 'Scheduled')
-group by aircraft_code),
-cte2 as (
-select COUNT(flight_no) over () as all_flights, aircraft_code 
-from bookings.flights
-where status not in ('Cancelled', 'Scheduled'))
-select distinct cte1.aircraft_code,  cte2.all_flights, cte1.real_flights, (cte1.real_flights*100/cte2.all_flights)::numeric as percents
-from cte1
-join cte2 using (aircraft_code)
-
-select bookings.tickets.book_ref, bookings.boarding_passes.boarding_no
-from bookings.tickets
-left join bookings.boarding_passes  using (ticket_no) 
-where bookings.boarding_passes.boarding_no  is null
